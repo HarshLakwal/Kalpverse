32b0df9448435e93e25eddaf304c71d372b5460d
harsh lakwal <85181214+HarshLakwal@users.noreply.github.com>
2023-07-12 14:17:39 +0530

    index update

--- index.js
+++ index.js
@@ -9,18 +9,18 @@ import cors from "cors";
 dotenv.config();
 dbConnection.DBConnection();
 
-const corsOptions = {
-  origin: "*",
-  credentials: true, //access-control-allow-credentials:true
-  optionSuccessStatus: 200,
-};
 const app = express();
 const dirname = path.resolve();
 app.use('/uploads', express.static(path.join(dirname, '/uploads')));
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use("/", mainRoute);
+const corsOptions = {
+  origin: 'http://localhost:3000',
+  credentials: true,            //access-control-allow-credentials:true
+  optionSuccessStatus: 200
+}
 app.use(cors(corsOptions));
+app.use("/", mainRoute);
 
 const server = app.listen(process.env.PORT, () => {
   console.log(`Server is running on ${process.env.PORT}🚀 `);
